7ab37238215c2ca5538f955dad09c72017ba122d
Raj kumar <study4810@gmail.com>
2025-11-17 23:15:39 +0530

    Arraays

---
+++ src/Arrays/Sorting.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Sorting {
+    public static void main(String[] args) {
+
+        int[] a = {1, 3, 95, 100, 5, 9, 10, 40};
+        String[] s = {"Raj", "Ravi", "Pawan", "Sandhya", "Shrawan"};
+
+        System.out.println(" Primitive Array elements before sorting ");
+        for (int a1 : a) {
+            System.out.println(" This is unsorted elements\n" + a1);
+
+        }
+        Arrays.sort(a);
+        System.out.println("primitive Array element after sorting ");
+        for (int a1 : a) {
+            System.out.println("This is sorted elements \n" + a1);
+        }
+        System.out.println("============Object Array==================");
+
+        System.out.println("Object Array before sorting ");
+        for (String s1 : s) {
+            System.out.println(s1);
+
+        }
+
+        Arrays.sort(s);
+        System.out.println("Object Array after Sorting");
+        for (String s1 : s) {
+            System.out.println(s1);
+        }
+
+        Arrays.sort(s, new SortingUsingComparator());
+        System.out.println("Object Array Afters Sorting using Comparator");
+        for (String a1: s){
+            System.out.println(a1);
+        }
+
+    }
+}
---
+++ src/Arrays/SortingUsingComparator.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+import java.util.Comparator;
+
+public class SortingUsingComparator implements Comparator {
+
+    public int compare(Object o1, Object o2){
+        String s1 = o1.toString();
+        String s2 = o2.toString();
+        return s2.compareTo(s1);
+    }
+}
